ad7a5006c4c319bd6b11f550b40cb317a6aadfea
1g0rbm <m1g0rb89@gmail.com>
2021-07-24 20:28:13 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+check: lint
+
+install:
+	npm ci
+
+lint:
+	npx eslint .
+lint-fix:
+	npx eslint . --fix
+
+build:
+	npm run build
+dev:
+	npm run dev
+watch:
+	npm run watch
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
