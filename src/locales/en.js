8d8e24d66f6c71289b83b4eb4867fc3993427a5d
1g0rbm <m1g0rb89@gmail.com>
2021-08-10 21:26:03 +0300

    Added en locale

---
+++ src/locales/en.js
@@ -0,0 +1,18 @@
+const en = {
+  translation: {
+    feeds: 'Feeds',
+    feed_loaded: 'RSS successfully loaded',
+    posts: 'Posts',
+    view: 'View',
+    errors: {
+      unknown: 'Unknown error',
+      url: 'Link should be a valid URL',
+      required: 'Do not be a blank',
+      url_exist: 'RSS already exist',
+      internet: 'Network error',
+      invalid_rss: 'Resources does not contain a valid RSS',
+    },
+  },
+};
+
+export default en;
